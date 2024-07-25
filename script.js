070b146004641c382f295a3065cf6f7dd4742b7b
NickMavrias <up1084664@upnet.gr>
2024-07-25 20:29:03 +0300

    LOGIN
    desktop version done @OrestisMakris , prospatho kai gia mobile

--- script.js
+++ script.js
@@ -28,3 +28,27 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Login button clicked inside overlay');
     });
 });
+
+
+
+//savvatou login
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapperButton = document.querySelector(".logBT");
+    const popupOverlay = document.getElementById("popupOverlay");
+    const exitButton = document.getElementById("exitButton");
+
+    wrapperButton.addEventListener("click", () => {
+        popupOverlay.classList.add("active");
+    });
+
+    exitButton.addEventListener("click", () => {
+        popupOverlay.classList.remove("active");
+    });
+
+    // Close the popup if the overlay is clicked
+    popupOverlay.addEventListener("click", (e) => {
+        if (e.target === popupOverlay) {
+            popupOverlay.classList.remove("active");
+        }
+    });
+});
