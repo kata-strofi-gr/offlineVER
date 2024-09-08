527f75095a542e7b8f506a410447a940ebb723e0
Achilleas Villiotis <axillvs@gmail.com>
2024-09-08 03:32:16 +0300

    added global fetch categories and items

---
+++ fetch_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'db_config.php'; // Make sure this script connects to your database
+
+header('Content-Type: application/json');
+
+// Query to get items from the database
+$sql = "
+    SELECT *
+    FROM Category
+    ORDER BY CategoryName
+    ";
+    
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $items = [];
+    while ($row = $result->fetch_assoc()) {
+        $items[] = [
+            'CategoryName' => $row['CategoryName'],
+            'CategoryID' => $row['CategoryID']
+        ];
+    }
+    echo json_encode($items);
+} else {
+    echo json_encode([]);
+}
+
+$conn->close();
+?>
