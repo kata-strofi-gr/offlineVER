4ebbbfd2df28da082d9f5bea6bb962052966efe8
Achilleas Villiotis <axillvs@gmail.com>
2024-09-09 00:43:58 +0300

    added ability to sort tables

--- Citizen/citizen.js
+++ Citizen/citizen.js
@@ -82,6 +82,23 @@ requestSubmitButton.addEventListener('click', function(e) {
     createRequest(selectedItemName, numOfPeople);
 });
 
+// Table sorting!
+// https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work...
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    const tbody = table.querySelector('tbody');
+    Array.from(tbody.querySelectorAll('tr'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => tbody.appendChild(tr));
+})));
+
 /**
  * Mobile frontend  //todo: this doesn't do anything??
  */
